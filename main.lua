9715b4884cc35212f4210812da61294b38a74577
IzNescafe <slei53922@gmail.com>
2025-01-11 22:13:11 +0630

    setting up the menu screen

---
+++ main.lua
@@ -0,0 +1,74 @@
+Class = require 'libs/class'
+
+require 'libs/StateMachine'
+push = require 'libs/push'
+
+require 'states/BaseState'
+require 'states/MenuState'
+
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    math.randomseed(os.time())
+
+    love.window.setTitle('Buckshot Roulette')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT,WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+
+    love.window.setTitle('Buckshot Roulette')
+
+    gStateMachine = StateMachine {
+        ['menu'] = function() return MenuState() end
+    }
+    gStateMachine:change('menu')
+
+    love.keyboard.keysPressed = {}
+
+    love.mouse.buttonsPressed = {}
+end
+
+function love.resize(w,h)
+    push:resize(w,h)
+end
+
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.mousepressed(x, y, button)
+    love.mouse.buttonsPressed[button] = true
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
+
+function love.mouse.wasPressed(button)
+    return love.mouse.buttonsPressed[button]
+end
+
+function love.update(dt)
+    gStateMachine:update(dt)
+
+    love.keyboard.keysPressed = {}
+    love.mouse.buttonsPressed = {}
+end
+
+function love.draw()
+    push:start()
+
+    gStateMachine:render()
+
+    push:finish()
+end
---
+++ states/MenuState.lua
@@ -0,0 +1,14 @@
+MenuState = Class{__includes = BaseState}  
+
+
+function MenuState:init()
+
+end
+
+function MenuState:enter()
+    
+end
+
+function MenuState:render()
+    love.graphics.clear(100/255, 35/255, 132/255)
+end
